5d2037d08f497b6a495879c521b8e57d027856b6
hotabiswajeet <hotabiswajeet333@gmail.com>
2023-02-23 00:05:43 +0530

    removed tags attribute from test runner

--- src/test/java/com/petstore/runner/TestRunner.java
+++ src/test/java/com/petstore/runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features ="src/test/java/com/petstore/features",plugin = "json:target/jsonReports/cucumber-reports.json",glue = "com.petstore.stepdefinitions",dryRun = false,monochrome = true, tags="@DeletePurchaseOrder")
+@CucumberOptions(features ="src/test/java/com/petstore/features",plugin = "json:target/jsonReports/cucumber-reports.json",glue = "com.petstore.stepdefinitions",dryRun = false,monochrome = true)
 
 public class TestRunner {
 
